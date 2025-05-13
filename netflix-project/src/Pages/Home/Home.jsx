70271d1839012b792cabd4a0263f93413b792ac7
Nardos-26 <nardosteshome95@gmail.com>
2025-05-13 03:44:47 -0400

    rows

--- netflix-project/src/Pages/Home/Home.jsx
+++ netflix-project/src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
 import Header from '../../Components/Header/Header.jsx'
 import Footer from '../../Components/Footer/Footer.jsx'
 import Banner from '../../Components/Banner/Banner.jsx'
+import RowList from '../../Components/Rows/RowList/RowList.jsx'
+
 
 function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   )
